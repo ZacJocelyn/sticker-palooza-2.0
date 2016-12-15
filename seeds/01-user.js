96abcfe7c18dc796b7aaa87bd761fc653bbbe8c4
Cassandra Torske <tors2720@gmail.com>
2016-12-15 14:42:58 -0700

    first seed :tada:

---
+++ seeds/01-user.js
@@ -0,0 +1,16 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex.raw('delete from "user"; alter sequence user_id_seq restart wtih 3')
+    .then(function () {
+      const users = [{
+        id: 1,
+        email: 'mrkulwhip@gmail.com',
+        password: 'password'
+      },{
+        id: 2,
+        email: 'thegrumpygoose@icloud.com',
+        password: 'heybro'
+      }]
+    });
+};
