104c5c7f43c41d50c364e59688f690627d968827
ZacJocelyn <zacjocelyn@gmail.com>
2016-12-16 11:01:47 -0700

    :rocket: did the thing

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,13 @@
 // Update with your config settings.
-
+require('dotenv').config();
 module.exports = {
 
   development: {
     client: 'pg',
     connection: 'postgres://localhost/sticker-palooza'
-  },
+  }, production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true'
+  }
 
 };
