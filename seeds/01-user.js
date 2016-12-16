273c8daf25f08823b451a0020a93e938b9e97600
ZacJocelyn <zacjocelyn@gmail.com>
2016-12-16 15:46:24 -0700

    :sad: tried to fix imgs

--- seeds/01-user.js
+++ seeds/01-user.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex.raw('delete from "user"; alter sequence user_id_seq restart with 3')
+  return knex.raw('delete from "user"; alter sequence user_id_seq restart with 1')
     .then(function () {
       const users = [{
         id: 1,
